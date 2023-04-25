7c0c6f6d5e6b66c68dd5d68592938ad316836fa8
Pranav Waghmare <piyushw55@gmail.com>
2023-04-25 13:52:49 +0530

    [Pranav]Added:Ability to create Line comparision problem and Perform operations

---
+++ Line_Comparision_New/LineComparision.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision_New
+{
+    public class LineComparision
+    {
+        int x_One, x_Three, y_One, y_Two;
+        //Constructor
+        public LineComparision(int x_One, int x_Two, int y_One, int y_Two)
+        {
+            this.x_One = x_One;
+            this.x_Three = x_Two;
+            this.y_One = y_One;
+            this.y_Two = y_Two;
+        }
+        public void CompareTwoLines()
+        {
+            double length = Math.Sqrt(Math.Pow((x_Three - x_One), 2) + Math.Pow((y_Two - y_One), 2));
+            Console.WriteLine("Length of the line : "+length);
+        }
+    }
+}
---
+++ Line_Comparision_New/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Line_Comparision_New
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel Come To Line Comparision Problem");
+
+            LineComparision lineComparision = new LineComparision(15, 24, 17, 8);
+            lineComparision.CompareTwoLines();
+            Console.ReadKey();
+        }
+    }
+}
