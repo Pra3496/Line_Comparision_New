aaf697a0211253ec8b97eb9dad4bf640ac8a7dc9
Pranav Waghmare <piyushw55@gmail.com>
2023-04-25 14:11:59 +0530

    [Pranav]Added:Ability to create Line comparision problem and Perform operations

--- Line_Comparision_New/Program.cs
+++ Line_Comparision_New/Program.cs
@@ -6,19 +6,21 @@
         {
             Console.WriteLine("\nWel Come To Line Comparision Problem\n");
 
-            LineComparision lineOne = new LineComparision(15, 24, 17, 8);
+            LineComparision lineOne = new LineComparision(5, 1, 7, 4);
             double lineLengthOne = lineOne.CompareTwoLines();
 
-            LineComparision lineTwo = new LineComparision(25, 3, 15, 21);
+            LineComparision lineTwo = new LineComparision(15, 14, 17, 8);
             double lineLengthTwo = lineTwo.CompareTwoLines();
 
-            
-            if (lineLengthOne.Equals(lineLengthTwo))
-            { Console.WriteLine("Both lines are equal"); }
+
+            if (lineLengthOne.CompareTo(lineLengthTwo) == 0)
+                Console.WriteLine("Both lines are equal");
+            else if (lineLengthOne.CompareTo(lineLengthTwo) > 0)
+                Console.WriteLine("Lenght of line one : "+lineLengthOne+" is greater than lenght of line two : " + lineLengthTwo);
             else
-            { Console.WriteLine("Both lines are unequal"); }
+                Console.WriteLine("Lenght of line one : "+lineLengthOne+" is lesser than length of line two : "+lineLengthTwo);
             Console.ReadKey();
-          
+
         }
     }
 }
